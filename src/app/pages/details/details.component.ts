02e56317cc8a1f258a12cf699d254d1dc11c0b1d
Nh4ru <tristanfernandez@live.fr>
2022-04-20 14:01:15 +0200

    Déplacement de quelques composants dans le dossier page

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -13,7 +13,7 @@ export class DetailsComponent implements OnInit {
 
   itemId = '';
   itemInfo:any;
-  imgSrc = '';
+  imgSrc = './assets/images/loader.gif';
   gray = false;
   blur=0;
 
@@ -22,7 +22,7 @@ export class DetailsComponent implements OnInit {
     this.gray? this.imgSrc +='grayscale&' : null;
     this.blur>0? this.imgSrc += 'blur=' + this.blur : null;
   }
-  
+
   grayscale(){
     this.gray=!this.gray;
     this.generateSrc();
